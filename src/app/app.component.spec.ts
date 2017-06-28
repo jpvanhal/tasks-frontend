4f1d0820a777a6daf6866d466c7089b11610b8b3
Janne Vanhala <janne.vanhala@gmail.com>
2017-06-28 13:10:30 +0300

    Fix AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,47 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
+let fixture: ComponentFixture<AppComponent>;
+
+@Component({
+  selector: 'app-task-list',
+  template: '',
+})
+export class TaskListStubComponent {}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    TestBed
+      .configureTestingModule({
+        imports: [
+          MdToolbarModule,
+        ],
+        declarations: [
+          AppComponent,
+          TaskListStubComponent,
+        ],
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it('should render title in a md-toolbar tag', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('md-toolbar').textContent).toContain('Tasks');
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render task list', async(() => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('md-toolbar').textContent).toContain('Tasks');
   }));
 });
