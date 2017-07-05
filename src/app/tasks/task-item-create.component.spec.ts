b65280b04e43b21e8d5b483f121169ecaebe8ba7
Janne Vanhala <janne.vanhala@gmail.com>
2017-07-05 13:30:40 +0300

    Add tests for TaskItemCreateComponent

--- src/app/tasks/task-item-create.component.spec.ts
+++ src/app/tasks/task-item-create.component.spec.ts
@@ -1,25 +1,90 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MdInputModule, MdListModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TaskItemCreateComponent } from './task-item-create.component';
 
 describe('TaskItemCreateComponent', () => {
   let component: TaskItemCreateComponent;
   let fixture: ComponentFixture<TaskItemCreateComponent>;
+  let onCreate: jasmine.Spy;
+  let input;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        MdInputModule,
+        MdListModule,
+        NoopAnimationsModule,
+      ],
       declarations: [ TaskItemCreateComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    onCreate = jasmine.createSpy('onCreate');
     fixture = TestBed.createComponent(TaskItemCreateComponent);
     component = fixture.componentInstance;
+    component.create.subscribe(onCreate);
     fixture.detectChanges();
+    input = fixture.debugElement.query(By.css('input'));
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  describe('when the input has text', () => {
+    beforeEach(async(() => {
+      input.nativeElement.value = '  Buy food ';
+      input.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+    }));
+
+    describe('when pressing Enter key', () => {
+      beforeEach(async(() => {
+        input = fixture.debugElement.query(By.css('input'));
+        input.triggerEventHandler('keyup.enter');
+        fixture.detectChanges();
+      }));
+
+      it('should emit "create" event', () => {
+        expect(onCreate).toHaveBeenCalledWith('Buy food');
+      });
+
+      it('should clear the input', () => {
+        expect(input.nativeElement.value).toBe('');
+      });
+    });
+
+    describe('when pressing Escape key', () => {
+      beforeEach(async(() => {
+        input = fixture.debugElement.query(By.css('input'));
+        input.triggerEventHandler('keyup.escape');
+        fixture.detectChanges();
+      }));
+
+      it('should not emit "create" event', () => {
+        expect(onCreate).not.toHaveBeenCalled();
+      });
+
+      it('should clear the input', () => {
+        expect(input.nativeElement.value).toBe('');
+      });
+    });
+  });
+
+  describe('when the input does not have text', () => {
+    describe('when pressing Enter key', () => {
+      beforeEach(async(() => {
+        input = fixture.debugElement.query(By.css('input'));
+        input.triggerEventHandler('keyup.enter');
+        fixture.detectChanges();
+      }));
+
+      it('should not emit "create" event', () => {
+        expect(onCreate).not.toHaveBeenCalled();
+      });
+    });
   });
 });
