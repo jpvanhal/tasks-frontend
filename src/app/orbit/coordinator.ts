45f4ee561fdc0d2332ab1611b6ad3eac8ffad81b
Janne Vanhala <janne.vanhala@gmail.com>
2017-09-05 09:58:30 +0300

    Defer remote requests queue until coordinator is active
    This prevents remote requests to be sent before the coordinator has activated
    the appropriate error handling strategies.

--- src/app/orbit/coordinator.ts
+++ src/app/orbit/coordinator.ts
@@ -3,6 +3,7 @@ import { Source } from '@orbit/data';
 import Store from '@orbit/store';
 
 import { BackupSource } from './data-sources/backup';
+import { RemoteSource } from './data-sources/remote';
 
 export function createCoordinator(sources: Source[], strategies: Strategy[]) {
   return new Coordinator({ sources, strategies });
@@ -11,10 +12,20 @@ export function createCoordinator(sources: Source[], strategies: Strategy[]) {
 export function initializeCoordinator(coordinator: Coordinator) {
   return (): Promise<void> => {
     const backup: BackupSource = <BackupSource>coordinator.getSource('backup');
+    const remote: RemoteSource = <RemoteSource>coordinator.getSource('remote');
     const store: Store = <Store>coordinator.getSource('store');
+
+    remote.requestQueue.autoProcess = false;
 
     return backup.pull((q) => q.findRecords())
       .then((transform) => store.sync(transform))
-      .then(() => coordinator.activate());
+      .then(() => coordinator.activate())
+      .then(() => {
+        remote.requestQueue.autoProcess = true;
+        remote.requestQueue.process().catch(() => {
+          // FIXME: Swallow all rejections. The errors are handled in the pushFail strategy.
+          // See: https://github.com/orbitjs/orbit/issues/451
+        });
+      });
   }
 }
